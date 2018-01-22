714b0823452eecdf437ed0e712411fd0c2ec424d
Shunkai Xing <xingshunkai@qq.com>
2018-01-22 20:47:43 +0800

    提交CH1的代码

---
+++ TeachCode代码教学区/CH1_HIghGUI的简单入门/CH1.3_ImageDisplayByHighGUI.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+# 导入一张图像 模式为彩色图片
+img = cv2.imread('cat.jpg', cv2.IMREAD_COLOR)
+
+# 创建一个名字叫做 image_win的窗口
+cv2.namedWindow('image_win', cv2.WINDOW_NORMAL)
+# 在名字叫做 image_win的窗口下展示图像
+cv2.imshow('image_win',img)
+# 检测按下的按钮
+print("请按任意键关闭窗口")
+key_pressed = cv2.waitKey(0)
+print("Key Pressed : {}  == {}".format(key_pressed, chr(key_pressed)))
+# cv2.destroyAllWindows()
+cv2.destroyWindow('image_win')
---
+++ TeachCode代码教学区/CH1_HIghGUI的简单入门/CH1.3_WaitKeyDemo.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+# 导入一张图像 模式为彩色图片
+img = cv2.imread('cat.jpg', cv2.IMREAD_COLOR)
+# 这里我们只是演示导入 并不会展示图像
+
+cv2.imshow('image',img)
+# 等待按键摁下 最多1s钟
+key_pressed = cv2.waitKey(5000)
+print("有按键摁下或者已超时")
+if key_pressed >= 0:
+    print("Key Pressed : {}  == {}".format(key_pressed, chr(key_pressed)))
+else:
+    print("等待超过5s钟自动执行")
+
+# 关闭所有窗口
+cv2.destroyAllWindows()
+
+
+
+'''
+输出
+
+scorpion@tl ~/D/f/Image> python WaitKeyDemo.py 
+有按键摁下或者已超时
+等待超过5s钟自动执行
+
+scorpion@tl ~/D/f/Image> python WaitKeyDemo.py 
+有按键摁下或者已超时
+Key Pressed : 115  == s
+'''
