20bd7dc6cba4f272657880785deaea0f4f952c37
Shunkai Xing <xingshunkai@qq.com>
2018-01-26 01:54:59 +0800

    添加CH2.4滑稽

---
+++ TeachCode代码教学区/CH2_Draw图像绘制/CH2.4_ResizeImage.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+def imgResize(img, ratio = 1):
+    height, width = img.shape[:2]
+    res = cv2.resize(img,(int(ratio*width), int(ratio*height)), interpolation = cv2.INTER_CUBIC)
+    return res
+
+
+
+huaji = cv2.imread("20180126huaji.jpg")
+cv2.imwrite("smallhuaji.png",imgResize(huaji, 0.2))
